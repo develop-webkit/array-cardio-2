1e7a914efd9fcf286c66afa9943431f7429c2621
Nabeel Yaseen <nabeelyaseenggwp@gmail.com>
2024-01-30 16:49:05 +0500

    1.0

--- resourses/js/script.js
+++ resourses/js/script.js
@@ -20,6 +20,24 @@
   // Array.prototype.some() // is at least one person 19 or older?
   // Array.prototype.every() // is everyone 19 or older?
 
+console.log(people.some(person => ((new Date().getFullYear()) - person.year) > 19 ));
+
+console.log(people.every(person => ((new Date().getFullYear()) - person.year)) > 19);
+
+console.log(comments.find(comment => comment.id === 823423));
+
+const index = comments.findIndex(comment => comment.id === 823423); 
+
+//comments.splice(index,1);
+
+
+const newArr = [
+  ...comments.slice(0,index),
+  ...comments.slice(index+1)
+];
+
+console.table(newArr);
+
   // Array.prototype.find()
   // Find is like filter, but instead returns just the one you are looking for
   // find the comment with the ID of 823423
